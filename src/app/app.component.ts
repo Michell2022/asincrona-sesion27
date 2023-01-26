e663588b5398f9d34b9be6ee31dd5733744b511f
Michell2022 <sistemas.angel04@gmail.com>
2023-01-26 13:59:29 -0500

    Testing + Solucion de errores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,25 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+
   title = 'testing';
-  valor1!:number;
-  valor2!:number;
+  valor1!: number;
+  valor2!: number;
   operacion: string = "Eliga la operación";
-  resultado: number = 0;
+  resultado!: number;
+  error = "";
 
 
-  //PODRIA MEJORARSE USANDO SWTCH
+  // SEGUNDA OPCION USANDO SWITCH
   ejecutarOperacion(): void {
-    if (this.operacion == "suma") {
-      this.resultado = this.valor1 + this.valor2;
-    } else if (this.operacion == "resta") {
-      this.resultado = this.valor1 - this.valor2;
-    } else if (this.operacion == "division") {
-      this.resultado = this.valor1 / this.valor2;
-    } else if (this.operacion == "multiplicacion") {
-      this.resultado = this.valor1 * this.valor2;
+    this.error = "";
+    switch (this.operacion) {
+      case "suma": this.resultado = this.valor1 + this.valor2;
+        break;
+      case "resta": this.resultado = this.valor1 - this.valor2;
+        break;
+      case "division": this.resultado = this.valor1 / this.valor2;
+        break;  
+      case "multiplicacion": this.resultado = this.valor1 * this.valor2;
+        break; 
+      default:
+        this.error = "Debe seleccionar una operación";
+        break;
     }
+
+
+    if(this.valor1 == null || this.valor2 == null){
+      this.error = "Debe ingresar datos validos";
+    }
+
+      //PRIMERA OPCION USANDO ESTRUCTURA CONDICIONAL ANIDADA.
+      
+    // if (this.operacion == "suma") {
+    //   this.resultado = this.valor1 + this.valor2;
+    // } else if (this.operacion == "resta") {
+    //   this.resultado = this.valor1 - this.valor2;
+    // } else if (this.operacion == "division") {
+    //   this.resultado = this.valor1 / this.valor2;
+    // } else if (this.operacion == "multiplicacion") {
+    //   this.resultado = this.valor1 * this.valor2;
+    // }
   }
 
 }
